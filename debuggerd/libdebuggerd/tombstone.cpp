ca025ff45f5e4098c3cff19e14a494b1008b6766
Pranav Vashi <neobuddy89@gmail.com>
2018-07-23 22:48:16 +0530

    tombstone: Get new one
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- debuggerd/libdebuggerd/tombstone.cpp
+++ debuggerd/libdebuggerd/tombstone.cpp
@@ -210,13 +210,13 @@ static const char* get_sigcode(int signo, int code) {
 static void dump_header_info(log_t* log) {
   char fingerprint[PROPERTY_VALUE_MAX];
   char revision[PROPERTY_VALUE_MAX];
-  char lineage_version[PROPERTY_VALUE_MAX];
+  char crdroid_version[PROPERTY_VALUE_MAX];
 
-  property_get("ro.lineage.version", lineage_version, "unknown");
+  property_get("ro.crdroid.version", crdroid_version, "unknown");
   property_get("ro.build.fingerprint", fingerprint, "unknown");
   property_get("ro.revision", revision, "unknown");
 
-  _LOG(log, logtype::HEADER, "LineageOS Version: '%s'\n", lineage_version);
+  _LOG(log, logtype::HEADER, "crDroid Version: '%s'\n", crdroid_version);
   _LOG(log, logtype::HEADER, "Build fingerprint: '%s'\n", fingerprint);
   _LOG(log, logtype::HEADER, "Revision: '%s'\n", revision);
   _LOG(log, logtype::HEADER, "ABI: '%s'\n", ABI_STRING);
